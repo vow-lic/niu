0e3a0a8cc17cc50fd8917476b6616d2824d961cb
vow_lic <lic5618@163.com>
2017-06-19 16:47:20 +0800

    picture get and post

--- niuweb.py
+++ niuweb.py
@@ -6,16 +6,23 @@ Created on Fri Jun  9 12:52:45 2017
 @author: lic
 """
 import os
-from flask import Flask,render_template,request
+import sys
+import sqlite3
+from flask import Flask,render_template,request,flash
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import Required
-import sqlite3
+from werkzeug.utils import secure_filename
 from flask_bootstrap import Bootstrap
+
+reload(sys)  
+sys.setdefaultencoding('utf8') 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'apmfeuasdfghjklzxcvbnm'
 app.config['DATABASE'] = os.path.join(app.root_path, 'niupinzhong.db')
+app.config['UPLOAD_FOLDER'] = os.path.join(app.root_path,'static')
+ALLOWED_EXTENSIONS = set(['jpg','png'])
 bootstrap = Bootstrap(app)
 
 class SeachForm(FlaskForm):
@@ -28,12 +35,17 @@ class AddForm(FlaskForm):
     bloodline = StringField(u'血缘',validators=[Required()])
     traits = StringField(u'性状',validators=[Required()])
     submit = SubmitField(u'提交')
-    
+ 
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS    
+
 @app.route('/',methods=['POST','GET'])
 def index(): 
     HABITAT = None
     BLOODLINE = None
     TRAITS = None
+    IMGSITE = None
     form = SeachForm()
     if request.method == 'POST':
         #name = form.name.data
@@ -48,8 +60,11 @@ def index():
             HABITAT = row[0][1]
             BLOODLINE = row[0][2]
             TRAITS = row[0][3]
+            if row[0][4]:
+                IMG = row[0][4]
+                IMGSITE = os.path.join('static',IMG)
             return render_template('index.html',form=form,name=NAME,habitat=HABITAT,\
-                           bloodline=BLOODLINE,traits=TRAITS)
+                           bloodline=BLOODLINE,traits=TRAITS,imgsite=IMGSITE)
         else:
             return render_template('index.html',data=None,form=form)
         cursor.close()
@@ -67,7 +82,17 @@ def add():
         habitat = request.form['habitat']
         bloodline = request.form['bloodline']
         traits = request.form['traits']
-        print name,habitat,bloodline,traits
+        if 'file' in request.files:
+            file = request.files['file']
+            if file.filename == '':
+                flash('No selected file')
+                print 'No selected file'
+            else :
+                if file and allowed_file(file.filename):
+                    filename = secure_filename(file.filename)
+                    file.save(os.path.join(app.config['UPLOAD_FOLDER'],filename))
+                    imgsite = filename
+        print name,habitat,bloodline,traits,imgsite
         conn = sqlite3.connect(app.config['DATABASE'])
         cursor = conn.cursor()
         values = cursor.execute('SELECT * FROM jianjie WHERE NAME=?',[name])
@@ -76,8 +101,8 @@ def add():
             print 'delete old data'
             cursor.execute('DELETE FROM jianjie WHERE NAME=?',[name])
         print 'write new data'
-        cursor.execute('INSERT INTO jianjie (name,habitat,bloodline,traits) VALUES (?,?,?,?)',\
-               [name,habitat,bloodline,traits])
+        cursor.execute('INSERT INTO jianjie (NAME,HABITAT,BLOODLINE,TRAITS,IMG) VALUES (?,?,?,?,?)',\
+               [name,habitat,bloodline,traits,imgsite])
         conn.commit()
     return render_template('add.html',form=form)
     print 'add over'
